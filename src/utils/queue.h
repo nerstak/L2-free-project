46b04b6857166afd4a497feb3761bbdf24e44125
Clément Weingaertner <clem.weingaertner@gmail.com>
2018-11-09 14:19:16 +0100

    Add queue (WIP) (Dungeon)

---
+++ src/utils/queue.h
@@ -0,0 +1,24 @@
+#ifndef FREE_PROJECT_TEST_QUEUE_H
+#define FREE_PROJECT_TEST_QUEUE_H
+
+#include "treemap.h"
+
+typedef struct QueueNode {
+    TreeMapNode* data;
+
+    struct QueueNode* next;
+} QueueNode;
+
+typedef struct Queue {
+    QueueNode* front;
+    QueueNode* rear;
+} Queue;
+
+extern Queue* init_Queue();
+extern void clean_Queue(Queue** myQueue);
+extern QueueNode* init_QueueNode(TreeMapNode* myTreeMapNode);
+extern void enQueue_Queue(Queue* myQueue, TreeMapNode* myNode);
+extern TreeMapNode* deQueue_Queue(Queue* myQueue);
+extern bool isEmpty_Queue(Queue* myQueue);
+
+#endif //FREE_PROJECT_TEST_QUEUE_H
