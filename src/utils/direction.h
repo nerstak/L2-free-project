292614ad4abf456e0251b9bc390734e2af5c5f71
Clément Weingaertner <clem.weingaertner@gmail.com>
2018-11-09 14:17:56 +0100

    Add direction (WIP) (Dungeon)

---
+++ src/utils/direction.h
@@ -0,0 +1,27 @@
+#ifndef FREE_PROJECT_TEST_DIRECTION_H
+#define FREE_PROJECT_TEST_DIRECTION_H
+
+/**
+ * N:0 0 -1
+ * E:1 1 0
+ * S:2 0 1
+ * W:3 -1 0
+ */
+
+ /**
+  * 4 Direction possible
+  */
+
+typedef struct Direction {
+    int code;
+    int x;
+    int y;
+} Direction;
+
+extern Direction* init_Direction(int code, int x, int y);
+extern void clean_Direction(Direction** myDirection);
+
+extern Direction* fromCode_Direction(int code);
+extern Direction* opposite_Direction(Direction* myDirection);
+
+#endif //FREE_PROJECT_TEST_DIRECTION_H
