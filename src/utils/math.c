d769194847e80bf7b97b32b5589a37fe61363845
Clément Weingaertner <clem.weingaertner@gmail.com>
2018-11-22 13:14:08 +0100

    Update math lib to add min fn

--- src/utils/math.c
+++ src/utils/math.c
@@ -3,3 +3,7 @@
 extern int max(int x, int y) {
     return (x >= y) ? x: y;
 }
+
+extern int min(int x, int y) {
+    return (x <= y) ? x: y;
+}
--- src/utils/math.h
+++ src/utils/math.h
@@ -2,5 +2,6 @@
 #define FREE_PROJECT_UTILS_MATH_H
 
 extern int max(int x, int y);
+extern int min(int x, int y);
 
 #endif //FREE_PROJECT_UTILS_MATH_H
