732e398ececa7ccd7996a7f7475a9064ce44d64f
theonogo <36893651+theonogo@users.noreply.github.com>
2018-11-25 01:08:03 +0100

    Pollevent and quitting adjustment

--- src/scenes/shopScreen/event.c
+++ src/scenes/shopScreen/event.c
@@ -1,35 +1,42 @@
 #include "event.h"
 
 extern void eventProcess_Scene_shop(SDL_Event event, Engine* engine, Data* data) {
-    switch (event.type) {
-        case SDL_KEYDOWN: {
-            // Key pressed
-            switch (event.key.keysym.sym) {
-                case SDLK_UP:
-                    data->shop->askAction = -10;
-                    break;
-                case SDLK_DOWN:
-                    data->shop->askAction = 10;
-                    break;
-                case SDLK_LEFT:
-                    data->shop->askAction = -1;
-                    break;
-                case SDLK_RIGHT:
-                    data->shop->askAction = 1;
-                    break;
-                case SDLK_RETURN:
-                    data->shop->askAction = 5;
-                    break;
-                default: {
-
-                    break;
+    if(SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_KEYDOWN: {
+                // Key pressed
+                switch (event.key.keysym.sym) {
+                    case SDLK_UP:
+                        data->shop->askAction = -10;
+                        break;
+                    case SDLK_DOWN:
+                        data->shop->askAction = 10;
+                        break;
+                    case SDLK_LEFT:
+                        data->shop->askAction = -1;
+                        break;
+                    case SDLK_RIGHT:
+                        data->shop->askAction = 1;
+                        break;
+                    case SDLK_RETURN:
+                        data->shop->askAction = 5;
+                        break;
+                    case SDLK_ESCAPE:
+                        data->stop= 0;
+                        break;
+                    default:
+                        break;
                 }
+                break;
             }
-            break;
-        }
-        default: {
-            //data->shop->askAction = 0;
-            break;
+            case SDL_QUIT: {
+                    data->stop = 0;
+                    break;
+            }
+            default: {
+                //data->shop->askAction = 0;
+                break;
+            }
         }
     }
 }
